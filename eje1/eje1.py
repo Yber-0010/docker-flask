d4af142759bf70dba431e9f7b9620f309f6967b2
Iber Chambi <iber65525526@gmail.com>
2023-05-15 07:53:35 -0400

    second commit

--- eje1/eje1.py
+++ eje1/eje1.py
@@ -1,7 +1,19 @@
-n1 = input("¿Dime el primer número?")
+import sys
+import os
+print ("ejercicio de suma dos numeros  python en docker ")
+""" en local """
+""" n1 = input("¿Dime el primer número?")
 n1 = int(n1)
 n2 = input("¿Dime el segundo número?")
-n2 = int(n2)
+n2 = int(n2) """
+""" como argumentos  """
+""" n1 = int(sys.argv[1])
+n2 = int(sys.argv[2]) """
+""" como variables de entorno """
+""" n1 = int(os.environ.get('NUM1'))
+n2 = int(os.environ.get('NUM2')) """
+n1 = 2
+n2 = 2
 suma = n1 + n2
 resta = n1 - n2
 mul = n1 * n2
